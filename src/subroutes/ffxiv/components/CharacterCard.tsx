abed1b2a1c8af65d5a7fb582bd7dfd1657cd304b
Ryan Clark <clar1513@umn.edu>
2022-09-15 15:53:32 -0500

    Creating job icon component

--- src/subroutes/ffxiv/components/CharacterCard.tsx
+++ src/subroutes/ffxiv/components/CharacterCard.tsx
@@ -1,3 +1,5 @@
+import JobIcon from "./JobIcon";
+
 function CharacterCard(props: any) {
   return (
     <div className="character-card" data-color={props.color} style={{borderLeft: `5px solid ${props.color}`}}>
@@ -5,7 +7,7 @@ function CharacterCard(props: any) {
       <div className="character-info">
         <div className="character-header">
           <h1>{props.firstName} {props.lastName}</h1>
-          <div className="character-job" data-job={props.job} /> 
+          <JobIcon job={props.job} />
         </div>
         <p>{props.description}</p>
       </div>
---
+++ src/subroutes/ffxiv/components/JobIcon.tsx
@@ -0,0 +1,35 @@
+
+import PLD from "../../../res/img/job_pld.png";
+import WAR from "../../../res/img/job_war.png";
+import DRK from "../../../res/img/job_drk.png";
+import GNB from "../../../res/img/job_gnb.png";
+import WHM from "../../../res/img/job_whm.png";
+import SCH from "../../../res/img/job_sch.png";
+import AST from "../../../res/img/job_ast.png";
+import SGE from "../../../res/img/job_sge.png";
+import MNK from "../../../res/img/job_mnk.png";
+import DRG from "../../../res/img/job_drg.png";
+import NIN from "../../../res/img/job_nin.png";
+import SAM from "../../../res/img/job_sam.png";
+import RPR from "../../../res/img/job_rpr.png";
+import BRD from "../../../res/img/job_brd.png";
+import MCH from "../../../res/img/job_mch.png";
+import DNC from "../../../res/img/job_dnc.png";
+import BLM from "../../../res/img/job_blm.png";
+import SMN from "../../../res/img/job_smn.png";
+import RDM from "../../../res/img/job_rdm.png";
+
+const IMAGE_MAP: {[key: string]: string} = {
+    PLD, WAR, DRK, GNB,
+    WHM, SCH, AST, SGE,
+    MNK, DRG, NIN, SAM, RPR,
+    BRD, MCH, DNC,
+    BLM, SMN, RDM
+};
+
+
+function JobIcon(props: any) {
+    return <div className="character-job" style={{backgroundImage: `url("${IMAGE_MAP[props.job]}")`}} />
+}
+
+export default JobIcon;
