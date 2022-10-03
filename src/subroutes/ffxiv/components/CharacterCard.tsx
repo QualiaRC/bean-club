f7be3029024ac12f2703f018fe466620263a16c9
Ryan Clark <clar1513@umn.edu>
2022-10-02 20:18:49 -0500

    I hate small screens

--- src/subroutes/ffxiv/components/CharacterCard.tsx
+++ src/subroutes/ffxiv/components/CharacterCard.tsx
@@ -2,14 +2,16 @@ import JobIcon from "./JobIcon";
 
 function CharacterCard(props: any) {
   return (
-    <div className="character-card" data-color={props.color} style={{borderLeft: `5px solid ${props.color}`}}>
-      <div className="character-image" data-crown={props.firstName === "Zenith" && props.lastName === "Wright" ? "" : undefined} style={{backgroundImage: `url("${props.image}")`}}></div>
+    <div className="character-card" data-color={props.color} style={{ borderLeft: `5px solid ${props.color}` }}>
+      <div className="character-image" data-crown={props.firstName === "Zenith" && props.lastName === "Wright" ? "" : undefined} style={{ backgroundImage: `url("${props.image}")` }}></div>
       <div className="character-info">
         <div className="character-header">
           <h1>{props.firstName} {props.lastName}</h1>
           <JobIcon job={props.job} />
         </div>
-        <p>{props.description}</p>
+        <div className="character-description">
+          {props.description}
+        </div>
       </div>
     </div>
   )
