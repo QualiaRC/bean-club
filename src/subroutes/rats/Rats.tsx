2db3f2a9dcbb1cbf5b3a7c185c339c722a273daf
Ryan Clark <clar1513@umn.edu>
2023-09-10 13:29:30 -0500

    They locked me in a room. A CSS room. A CSS room filled with styles.

--- src/subroutes/rats/Rats.tsx
+++ src/subroutes/rats/Rats.tsx
@@ -3,8 +3,8 @@ import { useEffect, useState } from 'react';
 import './Rats.css';
 
 function Rats() {
-  const ratSize = [100, 50, 50, 25, 25, 12.5, 12.5, 6.25, 6.25];
-  const numbers = ["one", "two", "four", "eight", "sixteen", "thirty two", "sixty four", "one hundred twenty eight", "two hundrd fifty six", "five hundred twelve", "one thousand twenty four", "two thousand forty eight"];
+  const ratSize = [100, 50, 50, 25, 25, 12.5, 12.5, 6.25, 6.25, 3.125, 3.125, 1.5625, 1.5625];
+  const numbers = ["one", "two", "four", "eight", "sixteen", "thirty two", "sixty four", "one hundred twenty eight", "two hundrd fifty six", "five hundred twelve", "one thousand twenty four", "two thousand forty eight", "four thousand ninety six"];
   const [numRats, setRats] = useState(0);
 
   const [dimensions, setDimensions] = useState(Math.min(window.innerHeight, window.innerWidth));
@@ -19,14 +19,13 @@ function Rats() {
     return () => { window.removeEventListener('resize', handleResize) }
   });
 
-
   return (
     <div style={{margin: 'auto', width: 'fit-content'}}>
-      <div className="rat-container" style={{width: `calc(${dimensions}px - 3em)`, height: `calc(${dimensions}px - 3em  )`}}>
+      <div className="rat-container" style={{width: `calc(${dimensions-5}px - 4em)`, height: `calc(${dimensions-5}px - 4em)`}}>
       {[...Array(2 ** numRats).keys()].map((_, i) => {
           return (
             <div className="rat-element-container" key={`rat-${i}`} style={{flexBasis: `${ratSize[numRats]}%`, height: `${ratSize[numRats]}%`}}>
-              <div className="rat-label" style={{fontSize: `calc(1.5em * ${ratSize[numRats] / 100})`}}>
+              <div className="rat-label" style={{fontSize: `calc(3em * ${ratSize[numRats] / 100})`}}>
                 {`${numbers[numRats]} rat${numRats === 0 ? '' : 's'}`}
               </div>
               <div className="rat-element" style={{backgroundImage: `url(${ImgRat})`}}></div>
@@ -34,7 +33,7 @@ function Rats() {
           )
         })}
       </div>
-      <input type="range" min="0" max="8" value={numRats} onChange={(e) => setRats(Number(e.target.value))} className="rats" id="input-rats"/> 
+      <input type="range" min="0" max="12" value={numRats} onChange={(e) => setRats(Number(e.target.value))} className="rats" id="input-rats"/> 
     </div>
   );
 }
