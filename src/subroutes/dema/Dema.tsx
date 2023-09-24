af2e801679d1c5abce7d558c0dbb719e7eddfbbe
Ryan Clark <ryan@rclark.dev>
2023-09-24 12:12:58 -0500

    Adding bg image uploading to dema module, adjusting CSS and logic

--- src/subroutes/dema/Dema.tsx
+++ src/subroutes/dema/Dema.tsx
@@ -1,74 +1,72 @@
 import "./Dema.css";
+import templateTransparent from "./demasign.png";
 import template from "./demasignwhite.png";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import Cropper, { ReactCropperElement } from "react-cropper";
+import "cropperjs/dist/cropper.css";
 
 function Dema() {
-    let canvas: HTMLCanvasElement;
-    let ctx: any = null;
-    let img: any = null;
-
+    const [mode, setMode] = useState("text");
     const [text, setText] = useState("I really\nlove beans!");
     const [fontSize, setFontSize] = useState(36);
     const [textPosition, setTextPosition] = useState([175, 220]);
     const [lineHeight, setLineHeight] = useState(1);
+    const [croppedBackground, setCroppedBackground] = useState(new Image());
+    const [showModal, setShowModal] = useState(false);
+    const [cropper, setCropper] = useState(<></>);
+    const cropperRef = useRef<ReactCropperElement>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
 
     useEffect(() => {
         document.body.style.backgroundColor = "#1e1e1e";
         document.body.style.color = "#ccc";
     });
 
-    useEffect(() => {
-        canvas = document.getElementById("dema-canvas") as HTMLCanvasElement;
-        ctx = canvas.getContext("2d");
+    const updateCanvas = () => {
+
+        const ctx = canvasRef.current?.getContext("2d");
 
         if (!ctx) return;
         ctx.font = `bold ${fontSize}px Arial`;
         ctx.textAlign = "center";
-        ctx.fillStyle = "black"
+        ctx.fillStyle = "black";
 
-        img = new Image();
-        img.src = template;
-        img.onload = () => update(text);
-    }, [text, fontSize, textPosition, lineHeight]);
+        const img = new Image();
+        img.src = croppedBackground.src ? templateTransparent : template;
 
-    const update = (t: string) => {
-        setText(t);
-        if (!ctx) return;
-        ctx.clearRect(0, 0, 300, 300);
-        ctx.drawImage(img, 0, 0);
+        img.onload = () => {
+            ctx.clearRect(0, 0, 300, 300);
 
-        ctx.save();
-        ctx.rotate(Math.PI * 0.058);
+            // Draw whatever background exists
+            if (croppedBackground.src) {
+                ctx.save();
+                ctx.rotate(Math.PI * 0.058);
+                ctx.drawImage(croppedBackground, 50, 140, 245, 135);
+                ctx.restore();
+            }
 
-        const lines = t.split('\n');
+            // Draw the Dema
+            ctx.drawImage(img, 0, 0);
 
-        let y = textPosition[1] - (lines.length - 1) * (fontSize * lineHeight / 2);
-        for (const line of lines) {
-            ctx.fillText(line, textPosition[0], y);
-            y += fontSize * lineHeight;
+            // Draw the text
+            ctx.save();
+            ctx.rotate(Math.PI * 0.058);
+            const lines = text.split('\n');
+            let y = textPosition[1] - (lines.length - 1) * (fontSize * lineHeight / 2);
+            for (const line of lines) {
+                ctx.fillText(line, textPosition[0], y);
+                y += fontSize * lineHeight;
+            }
+            ctx.restore();
         }
-        ctx.restore();
     }
 
-    const updateFontSize = (n: number) => {
-        setFontSize(n);
-        update(text);
-    }
+    useEffect(updateCanvas, [text, fontSize, textPosition, lineHeight, croppedBackground]);
 
-    const updatePosition = (x: number, y: number) => {
-        setTextPosition([x, y]);
-        update(text);
-    }
-
-    const updateLineHeight = (n: number) => {
-        setLineHeight(n);
-        update(text);
-    }
-
-    const save = (e: any) => {
+    const save = () => {
         const download = document.createElement("a");
         download.download = `dema.png`;
-        download.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+        download.href = canvasRef.current?.toDataURL("image/png").replace("image/png", "image/octet-stream") || "";
         download.click();
         download.remove();
     }
@@ -76,47 +74,102 @@ function Dema() {
     let to: NodeJS.Timeout;
     const copy = (e: any) => {
         if (to) clearTimeout(to);
-        canvas.toBlob((blob: any) => { navigator.clipboard.write([new ClipboardItem({ "image/png": blob })]) });
+        canvasRef.current?.toBlob((blob: any) => { navigator.clipboard.write([new ClipboardItem({ "image/png": blob })]) });
         e.target.innerText = "Copied!";
         to = setTimeout(() => e.target.innerText = "Copy", 1000);
     }
 
+    const upload = () => {
+        const uploader = document.createElement("input");
+        uploader.setAttribute("type", "file");
+        uploader.setAttribute("accept", "image/png, image/jpeg");
+        uploader.click();
+
+        uploader.addEventListener("change", function (e) {
+            if (!this.files?.length) return;
+
+            const fr = new FileReader()
+            fr.onload = function () {
+                setCropper(<Cropper src={(fr.result || "").toString()} style={{ height: "400px", width: "100%" }} aspectRatio={245 / 135} ref={cropperRef} />);
+                setShowModal(true);
+            }
+
+            fr.readAsDataURL(this.files[0]);
+        });
+    }
+
+    const finalizeCrop = () => {
+        const img = new Image();
+        img.src = cropperRef.current?.cropper.getCroppedCanvas().toDataURL() || "";
+        setCroppedBackground(img);
+        setShowModal(false);
+    }
+
+    const clear = () => { setCroppedBackground(new Image()) }
+
     return (
-        <div className="dema-container">
-            <canvas id="dema-canvas" width="300" height="300" onContextMenu={(e: any) => { e.preventDefault() }} style={{background:`url(${template})`}} />
+        <>
+            <div className="dema-container">
+                <canvas id="dema-canvas" width="300" height="300" ref={canvasRef} onContextMenu={(e: any) => { e.preventDefault() }} style={{ background: `url(${croppedBackground.src ? templateTransparent : template})` }} />
 
-            <div>
-                <button className="dema-button" onClick={save}>Save</button>
-                <button className="dema-button" onClick={copy}>Copy</button>
+                <div>
+                    <button className="dema-button" onClick={save}>Save</button>
+                    <button className="dema-button" onClick={copy}>Copy</button>
+                </div>
+
+                <div className="dema-setting-change">
+                    <button onClick={() => setMode("text")} data-active={mode === "text"}>Text</button>
+                    <button onClick={() => setMode("image")} data-active={mode === "image"}>Image</button>
+                </div>
+                <div className="dema-parameters">
+                    {
+                        mode === "text" ?
+                            <>
+                                <div>
+                                    Text:
+                                    <textarea className="dema-input" value={text} onInput={(e: any) => { setText(e.target.value) }} />
+                                </div>
+
+                                <div>
+                                    Font Size:
+                                    <input className="dema-input" type="range" min="4" max="128" value={fontSize} onInput={(e: any) => { setFontSize(e.target.value) }} />
+                                </div>
+
+                                <div>
+                                    Text position:
+                                    <div className="dema-slider-container">
+                                        <span>X</span><input type="range" min="0" max="300" value={textPosition[0]} className="dema-input" onChange={(e: any) => { setTextPosition([e.target.value, textPosition[1]]) }} />
+                                    </div>
+                                    <div className="dema-slider-container">
+                                        <span>Y</span><input type="range" min="0" max="300" value={textPosition[1]} className="dema-input" onChange={(e: any) => { setTextPosition([textPosition[0], e.target.value]) }} />
+                                    </div>
+                                </div>
+
+                                <div>
+                                    Line height:
+                                    <input className="dema-input" type="range" min="0.2" max="5" step="0.05" value={lineHeight} onChange={(e: any) => { setLineHeight(e.target.value) }} />
+                                </div>
+                            </> : <></>
+                    }
+                    {
+                        mode === "image" ?
+                            <div className="dema-crop-buttons">
+                                <button className="dema-button" onClick={upload}>Upload</button>
+                                <button className="dema-button" onClick={clear}>Clear</button>
+                            </div> : <></>
+                    }
+                </div>
             </div>
 
-            <div className="dema-parameters">
+            <div id="dema-upload-modal" hidden={!showModal}>
+                {cropper}
                 <div>
-                    Text:<br />
-                    <textarea className="dema-input" value={text} onInput={(e: any) => { update(e.target.value) }} />
-                </div>
-                
-                <div>
-                    Font Size:<br />
-                    <input className="dema-input" type="range" min="4" max="128" value={fontSize} onInput={(e: any) => { updateFontSize(e.target.value) }} />
-                </div>
-
-                <div>
-                    Text position:<br />
-                    <div className="dema-slider-container">
-                        <span>X</span><input type="range" min="0" max="300" value={textPosition[0]} onChange={(e: any) => { updatePosition(e.target.value, textPosition[1]) }} /><br />
-                    </div>
-                    <div className="dema-slider-container">
-                        <span>Y</span><input type="range" min="0" max="300" value={textPosition[1]} onChange={(e: any) => { updatePosition(textPosition[0], e.target.value) }} /><br />
-                    </div>
-                </div>
-
-                <div>
-                    Line height:<br />
-                    <input className="dema-input" type="range" min="0.2" max="5" step="0.05" value={lineHeight} onChange={(e: any) => { updateLineHeight(e.target.value) }} />
+                    <button className="dema-button" onClick={() => setShowModal(false)}>Cancel</button>
+                    <button className="dema-button" onClick={finalizeCrop}>Upload</button>
                 </div>
             </div>
-        </div>
+
+        </>
     )
 }
 
